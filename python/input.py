ba5ceec6073871bc63a3da514211789912896101
prasadJeyapalan <pjeyapalan@cox.net>
2024-01-04 19:29:37 -0600

    read write

---
+++ python/input.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+
+name = input("Enter your name: ")
+
+print(f"My name is {name}" )
---
+++ python/readwrite.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+months = open('months.txt')
+
+print(months.readlines())
+
+months.close()
